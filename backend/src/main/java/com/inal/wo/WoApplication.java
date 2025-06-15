f7651ba81cda8b2f02d738c97393f1595fee72df
inal val.id <inal@val.id>
2025-06-15 19:13:06 +0700

    init project

---
+++ backend/src/main/java/com/inal/wo/WoApplication.java
@@ -0,0 +1,13 @@
+package com.inal.wo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/inal/wo/WoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inal.wo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
